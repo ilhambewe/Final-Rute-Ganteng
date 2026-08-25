f8f106c93b6544b320dc8c44bddbd4f5bfddc0f2
ilhambewe <ilhambagusw13@gmail.com>
2026-08-25 14:09:21 +0700

    menambah citra satelit dan data halte

--- rute-ganteng/src/main.js
+++ rute-ganteng/src/main.js
@@ -1,5 +1,6 @@
 import { Map } from 'maplibre-gl';
 
+
 const mapElement = document.createElement('div');
 mapElement.id = 'map';
 mapElement.style.height = "500px";
@@ -7,8 +8,50 @@ document.body.appendChild(mapElement);
 
 const map = new Map({
   container: 'map',
-  style: 'https://demotiles.maplibre.org/globe.json',
-  center: [112.1, -6.90],
-  zoom: 3,
-  attributionControl: false
+  style: {
+    version: 8,
+    sources: {
+      satellite: {
+        type: 'raster',
+        tiles: [
+          'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+        ],
+        tileSize: 256,
+        attribution: 'Tiles © Esri'
+      }
+    },
+    layers: [
+      {
+        id: 'satellite',
+        type: 'raster',
+        source: 'satellite'
+      }
+    ]
+  },
+  center: [112.0693543, -6.8984631],
+  zoom: 12,
+  attributionControl: true
+});
+
+map.on('load', () => {
+  fetch('/data/Halte.geojson')
+    .then((response) => response.json())
+    .then((halteData) => {
+      map.addSource('halte', {
+        type: 'geojson',
+        data: halteData
+      });
+
+      map.addLayer({
+        id: 'halte-points',
+        type: 'circle',
+        source: 'halte',
+        paint: {
+          'circle-radius': 6,
+          'circle-color': '#e63946',
+          'circle-stroke-width': 2,
+          'circle-stroke-color': '#ffffff'
+        }
+      });
+    });
 });
