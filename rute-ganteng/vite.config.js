f45fc2efcee98786e78622fe5ac5c463c6ec8947
ilhambewe <ilhambagusw13@gmail.com>
2026-08-25 23:32:20 +0700

    add github pages deployment

--- rute-ganteng/vite.config.js
+++ rute-ganteng/vite.config.js
@@ -7,14 +7,12 @@ const currentDirectory = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   base: '/Final-Rute-Ganteng/',
   build: {
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       input: {
         main: resolve(currentDirectory, 'index.html'),
         map: resolve(currentDirectory, 'map/index.html'),
       },
     },
-  },
-  optimizeDeps: {
-    exclude: ['maplibre-gl'],
   },
 });
